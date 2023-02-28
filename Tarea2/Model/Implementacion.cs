8f5ac760b8c9bbae9374b810a6171d384c831d81
Erazo2 <melaerazo35@gmail.com>
2023-02-27 19:37:57 -0600

    Agregar archivos de proyecto.

---
+++ Tarea2/Model/Implementacion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea2.Model
+{
+    class Implementacion
+    {
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public int Edad { get; set; }
+        public string correo { get; set; }
+    }
+}
---
+++ Tarea2/PagePrincipal.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea2.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tarea2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PagePrincipal : ContentPage
+    {
+        public PagePrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnInformacion_Clicked(object sender, EventArgs e)
+        {
+            var emple = new Implementacion
+            {
+
+                Nombres = txtnombre.Text,
+                Apellidos = txtapellido.Text,
+                Edad = 0,
+                correo = txtCorreo.Text,
+            };
+
+            var secondpage = new PageResultado();
+            secondpage.BindingContext = emple; 
+            Navigation.PushAsync(secondpage);
+
+        }
+
+        
+    }
+}
